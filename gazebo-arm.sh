a41e9e6a490a2d6ddb7055ba1e3ed27700f858c6
W.L. Douglas Teeple <doug@douglasteeple.com>
2018-06-11 10:55:35 -1000

    Some docs on log and plot file creation

--- gazebo-arm.sh
+++ gazebo-arm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#
+######################################################
 # Launch gazebo-arm in an xterm window
-#
+# Plus a little song and dance to get both 
+# a plot file (gazebo-arm.plt)
+# and a log file (gazebo-arm.log)
+######################################################
 xterm -e "cd $SD/RoboND-DeepRL-Project/build/aarch64/bin; (./gazebo-arm.sh | tee ../../../gazebo-arm.log) 3>&1 1>&2 2>&3 | tee ../../../gazebo-arm.plt"
