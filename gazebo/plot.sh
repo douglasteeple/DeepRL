57c9f4fcea1a1f67b2de44c3a61d335c4369e1b8
W.L. Douglas Teeple <doug@douglasteeple.com>
2018-06-13 08:23:50 -1000

    Update plot.sh

--- gazebo/plot.sh
+++ gazebo/plot.sh
@@ -1,17 +1,21 @@
 #! /usr/bin/gnuplot --persist
-#####################################################################
+####################################################################################
 #
 # Plot the RoboND-DeepRL parameter tuning results
 #				Douglas Teeple June 2018
 #
 # Columns:
 #         1           2        3          4             5             6
-# SuccessfulGrabs TotalRuns Accuracy LearningRate maxLearningRate LSTMSize
+# SuccessfulGrabs TotalRuns Accuracy LearningRate maxLearningRate Last100Accuracy
 #
-#####################################################################
-firstrow = system('head -1 gazebo-arm-64.plt')
+####################################################################################
+fname = 'gazebo-arm.plt'
+firstrow = system("head -1 ".fname)
 set xlabel word(firstrow, 2)
 set ylabel word(firstrow, 3)
+secondrow = system("awk 'FNR==2{print \$0}' ".fname)
+LearnRate = word(secondrow, 4)
+lstmsize = word(secondrow, 6)
 set yrange [0:1]
 set xrange [0:100]
 
@@ -21,14 +25,15 @@ set multiplot layout 2,2 rowsfirst title "Robo-ND Deep Q RL LSTM Size and Learni
 set macros
 POS  = "at graph 0.2,0.9 font ',8'"
 POS2 = "at graph 0.2,0.8 font ',8'"
-SKIPPER = "every ::1 using 2:3 title columnheader(7) with lines"
+SKIPPER = "every ::1 using 2:3 title ".LearnRate." with lines"
 
 do for [n in "64 128 256 512"] {
-	stats 'gazebo-arm-'.n.'.plt' using 3 every ::50 nooutput
+	fname = 'gazebo-arm-'.n.'.plt'
+	stats fname using 3 every ::50 nooutput
 	set label 1 'LSTM-'.n @POS
 	set label 2 sprintf("Max Acc: %0.2f Run: %d\n(after settling)", STATS_max, STATS_index_max) @POS2
 	set arrow 1 from STATS_index_max,STATS_max+0.1 to STATS_index_max,STATS_max linecolor rgb "red"
-	plot for [i=0:*] 'gazebo-arm-'.n.'.plt' index i @SKIPPER
+	plot for [i=0:*] fname index i @SKIPPER
 }
 
 unset multiplot
